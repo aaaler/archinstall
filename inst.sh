5f7390a9a47606cfeb8916b082e716e491aeaf5f
Alexey Nikiforov <nikalexey@gmail.com>
2018-04-15 02:36:50 +0300

    remove dbg, get and run chroot script

--- inst.sh
+++ inst.sh
@@ -6,15 +6,10 @@ echo "[Make FS]"
 mkfs.fat -F32 /dev/nvme0n1p1
 mkfs.ext4 /dev/nvme0n1p3
 mkfs.ext4 /dev/nvme0n1p4
-#dbg sleep
-read
-
 
 echo "[Make swap]"
 mkswap /dev/nvme0n1p2
 swapon
-#dbg sleep
-read
 
 echo "[Mount FS]"
 mount /dev/nvme0n1p3 /mnt
@@ -22,8 +17,6 @@ mkdir /mnt/boot
 mkdir /mnt/home
 mount /dev/nvme0n1p1 /mnt/boot
 mount /dev/nvme0n1p4 /mnt/home
-#dbg sleep
-read
 
 echo "[Set locale]"
 loadkeys ru
@@ -31,33 +24,36 @@ setfont cyr-sun16
 echo -e "en_US.UTF-8 UTF-8\nru_RU.UTF-8 UTF-8" > /etc/locale.gen
 locale-gen
 export LANG=ru_RU.UTF-8
-#dbg sleep
-read
 
 echo "[Set time]"
+timedatectl set-timezone Europe/Moscow
 timedatectl set-timezone Europe/Moscow
 timedatectl set-ntp true
 sleep 3
 timedatectl status
-sleep 3
-#dbg sleep
-read
 
 echo "[Set pacman mirror]"
 sed -i '1s/^/Server = http:\/\/mirror.yandex.ru\/archlinux\/$repo\/os\/$arch\n/' /etc/pacman.d/mirrorlist
-#dbg sleep
-read
 
 echo "[Base install]"
 pacman -Syy
 echo -e "\n\n" | pacstrap -i /mnt base base-devel
-#dbg sleep
-read
 
 echo "[Generated fatab]"
 genfstab -U -p /mnt >> /mnt/etc/fstab
-#dbg sleep
-read
+
+echo "[Install chroot script]"
+wget https://raw.githubusercontent.com/nikalexey/archinstall/master/chroot_inst.sh
+chmod +x chroot_inst.sh
+mkdir /mnt/install
+cp chroot_inst.sh /mnt/install/chroot_inst.sh
+
+echo "[Run chroot script]"
+arch-chroot /mnt /install/chroot_inst.sh
 
 echo "[Umount all]"
 umount -R /mnt
+
+echo "[Finish] Press Enter to reboot"
+read
+reboot
