22169dd6e2c81eed2e82b0e65e4e97895fff9132
Alexey Nikiforov <nikalexey@gmail.com>
2018-04-16 00:51:07 +0300

    Install pacaur

--- chroot_inst.sh
+++ chroot_inst.sh
@@ -4,7 +4,7 @@ echo "[Setup ArchLinux]"
 host_name="anikiforov_nb"
 user_name="anikiforov"
 pass_common="1"
-pacman_pkg="grub efibootmgr intel-ucode dnsutils xorg-server xorg-xinit xorg-iceauth xorg-sessreg xorg-xcmsdb xorg-xbacklight xorg-xgamma xorg-xhost xorg-xinput xorg-xmodmap xorg-xrandr xorg-xrdb xorg-xrefresh xorg-xset xorg-xsetroot mesa python2 git mc zsh openssh wget dialog wpa_supplicant awesome xf86-video-intel xf86-video-vesa xf86-video-fbdev xorg-fonts-cyrillic xorg-fonts-100dpi ttf-ubuntu-font-family slim"
+pacman_pkg="grub efibootmgr intel-ucode yajl expac dnsutils xorg-server xorg-xinit xorg-iceauth xorg-sessreg xorg-xcmsdb xorg-xbacklight xorg-xgamma xorg-xhost xorg-xinput xorg-xmodmap xorg-xrandr xorg-xrdb xorg-xrefresh xorg-xset xorg-xsetroot mesa python2 git mc zsh openssh wget dialog wpa_supplicant awesome xf86-video-intel xf86-video-vesa xf86-video-fbdev xorg-fonts-cyrillic xorg-fonts-100dpi ttf-ubuntu-font-family slim"
 
 
 echo "[Set locale and fonts]"
@@ -36,6 +36,30 @@ echo -e "$pass_common\n$pass_common" | passwd
 echo -e "$pass_common\n$pass_common" | passwd $user_name
 chsh -s /bin/zsh
 
+
+echo "[Install pacaur]"
+cd /tmp
+git clone https://aur.archlinux.org/package-query.git
+chown -R $user_name package-query
+cd package-query
+sudo -u $user_name makepkg
+pacman -U package-query-*.pkg.tar.xz --noconfirm
+
+gpg --recv-keys --keyserver hkp://keyserver.ubuntu.com 1EB2638FF56C0C53
+cd /tmp
+git clone https://aur.archlinux.org/cower.git
+chown -R $user_name cower
+cd cower
+sudo -u $user_name makepkg
+pacman -U cower-*.pkg.tar.xz --noconfirm
+
+cd /tmp
+git clone https://aur.archlinux.org/pacaur.git
+chown -R $user_name pacaur
+cd pacaur
+sudo -u $user_name makepkg
+pacman -U pacaur-*.pkg.tar.xz --noconfirm
+
 #dbg
 systemctl enable slim.service
 
@@ -43,6 +67,12 @@ echo "[Install GRUB]"
 grub-install --recheck /dev/sda --efi-directory=/boot
 sed -i 's/^GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/g' /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg
+
+
+echo "[Install home]"
+#dbg
+echo -e "exec awesome\n" > /home/$user_name/.xinitrc
+
 
 echo "[Finish Setup ArchLinux]"
 #read
