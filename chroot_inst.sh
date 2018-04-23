c4b84e43307f14562a0c455cea73f95addd391f3
Alexey Nikiforov <nikalexey@gmail.com>
2018-04-23 21:14:53 +0300

    lightdm nvidia

--- chroot_inst.sh
+++ chroot_inst.sh
@@ -4,8 +4,8 @@ echo "[Setup ArchLinux]"
 host_name="anikiforov_nb"
 user_name="anikiforov"
 pass_common="1"
-pacman_pkg="grub efibootmgr intel-ucode yajl expac dnsutils xorg-server xorg-xinit xorg-iceauth xorg-sessreg xorg-xcmsdb xorg-xbacklight xorg-xgamma xorg-xhost xorg-xinput xorg-xmodmap xorg-xrandr xorg-xrdb xorg-xrefresh xorg-xset xorg-xsetroot mesa python2 git mc zsh openssh wget dialog wpa_supplicant awesome xf86-video-intel xf86-video-vesa xf86-video-fbdev xorg-fonts-cyrillic xorg-fonts-100dpi ttf-ubuntu-font-family slim chromium arandr mesa-demos xsel ttf-droid ttf-dejavu"
-pacaur_pkg="oh-my-zsh-git rxvt-unicode-patched sublime-text-dev ttf-fira-code"
+pacman_pkg="grub efibootmgr intel-ucode yajl expac dnsutils xorg-server xorg-xinit xorg-iceauth xorg-sessreg xorg-xcmsdb xorg-xbacklight xorg-xgamma xorg-xhost xorg-xinput xorg-xmodmap xorg-xrandr xorg-xrdb xorg-xrefresh xorg-xset xorg-xsetroot mesa python2 git mc zsh openssh wget dialog wpa_supplicant awesome xf86-video-intel xf86-video-vesa xf86-video-fbdev xorg-fonts-cyrillic xorg-fonts-100dpi ttf-ubuntu-font-family lightdm lightdm-webkit2-greeter chromium arandr mesa-demos xsel ttf-droid ttf-dejavu xterm"
+pacaur_pkg="oh-my-zsh-git rxvt-unicode-patched sublime-text-dev ttf-fira-code zsh-syntax-highlighting"
 
 echo "[Set locale and fonts]"
 echo LANG=ru_RU.UTF-8 > /etc/locale.conf
@@ -17,6 +17,10 @@ loadkeys ru
 setfont cyr-sun16
 echo -e "KEYMAP=ru\nFONT=cyr-sun16\nFONT_MAP=\n" > /etc/vconsole.conf
 
+echo "[Settings pacman]"
+sed -i 's/#[multilib]/[multilib]/g' /etc/pacman.conf
+sed -i 's/#Include = /etc/pacman.d/mirrorlist/Include = /etc/pacman.d/mirrorlist/g' /etc/pacman.conf
+
 echo "[Set hostname]"
 echo $host_name > /etc/hostname
 
@@ -26,10 +30,9 @@ hwclock --systohc --utc
 
 echo "[Install package]"
 pacman -Syu
+pacman -S --noconfirm --needed dkms
 pacman -S --noconfirm --needed archlinux-keyring
 pacman -S --noconfirm --needed $pacman_pkg
-
-echo "[Install pacman]"
 
 echo "[Add user]"
 useradd -m -g users -G lp,optical,power,storage,video,audio,wheel -s /bin/zsh $user_name
@@ -64,8 +67,10 @@ pacman -U pacaur-*.pkg.tar.xz --noconfirm
 echo "[Install pacaur]"
 sudo -u $user_name pacaur -S --noconfirm --noedit  $pacaur_pkg
 
-#dbg
-systemctl enable slim.service
+echo "[System settings]"
+sed -i 's/^#greeter-session=.*/greeter-session=lightdm-webkit2-greeter/g' /etc/lightdm/lightdm.conf
+systemctl enable lightdm.service
+pacman -S nvidia-dkms
 
 echo "[Install GRUB]"
 grub-install --recheck /dev/sda --efi-directory=/boot
@@ -76,16 +81,15 @@ echo "[Install home]"
 cd /tmp
 git clone https://github.com/nikalexey/archconfig.git
 cp archconfig/etc/udev/hwdb.d/61-key-remap.hwdb /etc/udev/hwdb.d
-cp archconfig/home/.Xresources /home/$user_name/
-cp -r archconfig/home/.urxvt /home/$user_name/
-cp -r archconfig/home/.config /home/$user_name/
+cp -r archconfig/home/ /home/$user_name/
+chown $user_name:users /home/$user_name
 
 echo "[Setting udev]"
 udevadm hwdb --update
 udevadm trigger
 
 #dbg
-echo -e "exec awesome\n" > /home/$user_name/.xinitrc
+# echo -e "exec awesome\n" > /home/$user_name/.xinitrc
 
 
 echo "[Finish Setup ArchLinux]"
